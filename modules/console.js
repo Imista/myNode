2e6bdd658d70781aa43efdebabb571770dee8e2c
MuffinPlease <benjamintec3455@gmail.com>
2022-01-23 19:37:20 -0600

    Class 13 console

---
+++ modules/console.js
@@ -0,0 +1,29 @@
+// console.log('Some');
+// console.info('Some');
+// console.warn('Some');
+// console.error('Some');
+
+
+// let table = [
+//     {
+//         a: 1,
+//         b: 'Z'
+//     },
+//     {
+//         a: 2,
+//         b: 'Y'
+//     }
+// ]
+
+// console.table(table);
+
+// console.group('Conver');
+
+// console.log('Hello');
+// console.log('Blablabla');
+// console.log('Bye');
+
+// console.groupEnd('conver');
+
+// console.log('Some else')
+
